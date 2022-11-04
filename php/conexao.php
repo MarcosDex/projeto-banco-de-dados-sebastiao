d2871b09753760cb59a738b60296368265c73643
MarcosDex <marquinhospbongame@gmail.com>
2022-11-04 09:37:10 -0300

    Adicionado conexao com banco de dados

---
+++ php/conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$bdHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'cadastrobd';
+
+$conexao = new mysqli($bdHost, $dbUsername, $dbPassword, $dbName);
+
+if ($conexao->connect_errno) {
+    echo "Erro ao conetar banco de dados";
+} else {
+    echo "Conexão estabelecida com sucesso!";
+}
