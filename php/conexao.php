f1dac5e12ab8c18ba73acdd28c6e5c8bbaab2314
MarcosDex <marquinhospbongame@gmail.com>
2022-11-09 14:30:00 -0300

    Adicionado tela de registros

--- php/conexao.php
+++ php/conexao.php
@@ -7,8 +7,8 @@ $dbName = 'cadastrobd';
 
 $conexao = new mysqli($bdHost, $dbUsername, $dbPassword, $dbName);
 
-if ($conexao->connect_errno) {
-    echo "Erro ao conetar banco de dados";
-} else {
-    echo "Conexão estabelecida com sucesso!";
-}
+// if ($conexao->connect_errno) {
+//     echo "Erro ao conetar banco de dados";
+// } else {
+//     echo "Conexão estabelecida com sucesso!";
+// }
