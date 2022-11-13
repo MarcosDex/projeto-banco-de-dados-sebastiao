6ac8e6d20ab8f906b6cba5786713db5da66895a2
MarcosDex <marquinhospbongame@gmail.com>
2022-11-12 23:52:59 -0300

    att

--- index.php
+++ index.php
@@ -6,10 +6,36 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <title>Projeto Banco de Dados Sebastião</title>
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Unifalip Fyden</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+          <a class="nav-link" href="#">Ofertas do dia</a>
+          <a class="nav-link" href="#"></a>
+          <a class="nav-link"></a>
+        </div>
+      </div>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn">Area de Usabilidade</a>
+        <div class="dropdown-content">
+          <a href="../php/cadastra.php">Registrar</a>
+          <a href="../php/login.php">Login</a>
+        </div>
+      </li>
+      </ul>
+    </div>
+
+  </nav>
   <a href="../php/conexao.php">Bd</a>
   <a href="../php/cadastra.php">cadastra</a>
 </body>
