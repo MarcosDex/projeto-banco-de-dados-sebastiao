9134759d8840cea056fd2e9113456d9be755feb8
MakotoNoxx <thobias18@gmail.com>
2022-11-12 23:42:48 -0300

    Adicionado login
    Adicionado login.php e tela de login
    
    Co-Authored-By: WalkerAway <54985051+MarcosDex@users.noreply.github.com>
    Co-Authored-By: IrthyellMuniz <105610634+IrthyellMuniz@users.noreply.github.com>

---
+++ php/getlogin.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+//Se a o botão (submit), email e senha forem nulos redireciona pra area de login dnv
+if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])){
+//se o usuario preencher todos os campos ele acessa o sistema
+include_once('../php/conexao.php');
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+//print_r('Email: ' . $email);
+//print_r('<br>');
+//print_r('Senha: ' . $senha);
+
+$sql = "SELECT * FROM  usuarios WHERE email = '$email' and senha = '$senha'";
+
+$resultado = $conexao->query($sql);
+
+
+if(mysqli_num_rows($resultado) < 1) {
+    unset($_SESSION['email']);
+    unset($_SESSION['senha']);
+    header('Location: login.php');
+}else{
+    $_SESSION['email'] = $email;
+    $_SESSION['senha'] = $senha;
+    header('Location: sistema.php');
+}
+
+
+}else{
+//se o usuario n preencher todos ou nenhum campo n tem acesso ao sistema e retorna pra area de login novamente
+header('Location: login.php');
+}
+
+
+?>
