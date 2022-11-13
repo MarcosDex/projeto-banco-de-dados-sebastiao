1ee9154c3d24f2f6010015acd0808c14f5809297
MarcosDex <marquinhospbongame@gmail.com>
2022-11-13 14:40:37 -0300

    Teste
    Co-Authored-By: IrthyellMuniz <105610634+IrthyellMuniz@users.noreply.github.com>
    Co-Authored-By: MakotoNoxx <117718897+MakotoNoxx@users.noreply.github.com>

--- index.php
+++ index.php
@@ -38,7 +38,20 @@
 
   </nav>
   <section class="teams" id="teams">
+    <?php
+    session_start();
+    print_r($_SESSION);
+    if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
+      unset($_SESSION['email']);
+      unset($_SESSION['senha']);
+      header('Location: login.php');
+    } else {
+      $logado = $_SESSION['email'];
+    }
 
+    echo "Bem vindo $logado";
+
+    ?>
   </section>
 
 </body>
